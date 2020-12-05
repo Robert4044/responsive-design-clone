792aea24c22606a82ad84275f513fe5378e9ef76
Robert Martin <robert4044@gmail.com>
2020-12-04 20:57:15 -0600

    final

--- ressponsive-design/src/App.js
+++ ressponsive-design/src/App.js
@@ -83,7 +83,58 @@ function App() {
                 <p className='services-text'>
                     Lorem ipsum, dolor sit amet consectetur.
                 </p>
-                <div className='portfolio-cards'></div>
+                <section className='portfolio-cards'>
+                    <div className='single-cards'>
+                        <img
+                            src='https://images.unsplash.com/photo-1441986300917-64674bd600d8?ixlib=rb-1.2.1&ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&auto=format&fit=crop&w=1350&q=80'
+                            alt=''
+                        />
+
+                        <h2 className='port-title'>Threads</h2>
+                        <h3 className='port-desc'>Illustration</h3>
+                    </div>
+
+                    <div className='single-cards'>
+                        <img
+                            src='https://images.unsplash.com/photo-1505778276668-26b3ff7af103?ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=1486&q=80'
+                            alt=''
+                        />
+                        <h2 className='port-title'>Explore</h2>
+                        <h3 className='port-desc'>Graphic Design</h3>
+                    </div>
+                    <div className='single-cards'>
+                        <img
+                            src='https://images.unsplash.com/photo-1538332576228-eb5b4c4de6f5?ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=1350&q=80'
+                            alt=''
+                        />
+                        <h2 className='port-title'>Finish</h2>
+                        <h3 className='port-desc'>Identity</h3>
+                    </div>
+                    <div className='single-cards'>
+                        <img
+                            src='https://images.unsplash.com/photo-1416339134316-0e91dc9ded92?ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=1567&q=80'
+                            alt=''
+                        />
+                        <h2 className='port-title'>Lines</h2>
+                        <h3 className='port-desc'>Branding</h3>
+                    </div>
+                    <div className='single-cards'>
+                        <img
+                            src='https://images.unsplash.com/photo-1523726491678-bf852e717f6a?ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=1350&q=80'
+                            alt=''
+                        />
+                        <h2 className='port-title'>Southwest</h2>
+                        <h3 className='port-desc'>Website Design</h3>
+                    </div>
+                    <div className='single-cards'>
+                        <img
+                            src='https://images.unsplash.com/photo-1509573563917-a778dc0a5477?ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=800&q=80'
+                            alt=''
+                        />
+                        <h2 className='port-title'>Window</h2>
+                        <h3 className='port-desc'>Photography</h3>
+                    </div>
+                </section>
             </section>
         </div>
     )
